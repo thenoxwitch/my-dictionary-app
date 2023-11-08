423b25a1ed5f6be8a139e6c55fa78c1af09c103e
thenoxwitch <131623738+thenoxwitch@users.noreply.github.com>
2023-11-08 11:53:12 +0000

    created a new component Dictionary/Created function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logoatha.png';
 import './App.css';
-
+import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo imf-fluid" alt="logo" />
-       Hello World
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo imf-fluid" alt="logo" />
+        
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        
+        <footer className="text-center">
+          <small><a href="https://github.com/thenoxwitch/my-dictionary-app">
+            Open Source Project{" "}
+          </a>
+          Coded with 💜 by Atenas G.I.{" "}</small>
+        </footer>
+      </div>
     </div>
   );
 }
