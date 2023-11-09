efcbd699111760a248aead055e12699a926c99f7
thenoxwitch <131623738+thenoxwitch@users.noreply.github.com>
2023-11-09 10:40:52 +0000

    added axios and api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,15 +5,24 @@ import axios from "axios";
 export default function Dictionary() {
 let [keyword, setKeyword] = useState("");
     
+
+function handleResponse(response){
+    console.log(response);
+}
+
+
 function search(event) {
         event.preventDefault();
         alert(`Searching for ${keyword} definition`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
+
     }
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
     }
-
 
     return (
     <div className="Dictionary">
