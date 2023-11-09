9853defb6be0445702459833b74b4c95dd064b26
thenoxwitch <131623738+thenoxwitch@users.noreply.github.com>
2023-11-09 13:50:23 +0000

    added new components and functions to display meanings of words

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,34 +1,35 @@
 import React, { useState } from "react";
-import "./Dictionary.css"
+import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results.js";
 
 export default function Dictionary() {
-let [keyword, setKeyword] = useState("");
-    
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
-function handleResponse(response){
-    console.log(response);
-}
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    setResults(response.data[0]);
+  }
 
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
 
-function search(event) {
-        event.preventDefault();
-        alert(`Searching for ${keyword} definition`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-    }
-
-    function handleKeywordChange(event) {
-        setKeyword(event.target.value);
-    }
-
-    return (
+  return (
     <div className="Dictionary">
-        <form onSubmit={search}>
-            <input type="search" onChange={handleKeywordChange}/>
-        </form>
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
     </div>
-    );
+  );
 }
