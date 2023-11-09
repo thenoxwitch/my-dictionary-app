010eb304e7ae04009c54be647824444df19c0cb0
thenoxwitch <131623738+thenoxwitch@users.noreply.github.com>
2023-11-09 14:09:23 +0000

    removed alert on search function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,13 +8,13 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+   
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
